e88a368f69bddd3c04e9ab1db2846df5faa75942
Ichigo21pro <aiglegon@gmail.com>
2025-06-11 21:45:15 +0100

    chest open and drop items (bugs whit interact whit this objects)

--- Assets/_Main/Scripts/InteractChest/InteractObject.cs
+++ Assets/_Main/Scripts/InteractChest/InteractObject.cs
@@ -12,9 +12,12 @@ public class Chest : MonoBehaviour
     [SerializeField] PlayerAttack PlayerAttackScript;
     [SerializeField] MoneyScript MoneyScript;
 
+    public GameObject moneyPrefab;
+    public GameObject cargadorPrefab;
 
-
+    private bool isOpened = false;
     private bool isPlayerInside = false;
+
     void Start()
     {
         if (objectToShow != null)
@@ -23,64 +26,117 @@ public class Chest : MonoBehaviour
 
     void Update()
     {
-        if (triggerZone != null)
+        CheckPlayerInTrigger();
+
+        if (isPlayerInside)
         {
-            ContactFilter2D filter = new ContactFilter2D();
-            filter.useTriggers = true;
-            Collider2D[] results = new Collider2D[10];
-
-            int count = triggerZone.OverlapCollider(filter, results);
-
-            bool playerFound = false;
-            for (int i = 0; i < count; i++)
-            {
-                if (results[i] != null && results[i].CompareTag(playerTag))
-                {
-                    playerFound = true;
-                    break;
-                }
-            }
-
-            if (playerFound != isPlayerInside)
-            {
-                isPlayerInside = playerFound;
-                objectToShow?.SetActive(isPlayerInside);
-            }
-
-            if (isPlayerInside)
-            {
-                DoSomethingWhileInside(NameOpcion);
-            }
+            HandleInteraction(NameOpcion);
         }
     }
 
-    void DoSomethingWhileInside(string parametro)
+
+    // Verifica si el jugador está dentro del área de interacción
+    void CheckPlayerInTrigger()
     {
-        if (!string.IsNullOrEmpty(parametro))
-        {
-            if (parametro == "Chest" && Input.GetKeyDown(KeyCode.E))
-            {
+        if (triggerZone == null) return;
 
-            }
-            else if (parametro == "Pistol" && Input.GetKeyDown(KeyCode.E))
+        ContactFilter2D filter = new ContactFilter2D();
+        filter.useTriggers = true;
+        Collider2D[] results = new Collider2D[10];
+
+        int count = triggerZone.OverlapCollider(filter, results);
+
+        bool playerFound = false;
+        for (int i = 0; i < count; i++)
+        {
+            if (results[i] != null && results[i].CompareTag(playerTag))
             {
-                PlayerAttackScript.pistola = true;
-                PlayerAttackScript.yaTienePistola = true;
-                Destroy(gameObject);
-            }
-            else if (parametro == "Cargador" && Input.GetKeyDown(KeyCode.E) && PlayerAttackScript.pistola) {
-                PlayerAttackScript.cargadores = PlayerAttackScript.cargadores+1;
-                PlayerAttackScript.TextoTMPCargadores.text = "" + PlayerAttackScript.cargadores;
-                Destroy(gameObject);
-            }
-            else if(parametro == "Money" && Input.GetKeyDown(KeyCode.E))
-            {
-                MoneyScript.Instance.AddRandomMoney();
-                Destroy(gameObject);
+                playerFound = true;
+                break;
             }
         }
+
+        if (playerFound != isPlayerInside)
+        {
+            isPlayerInside = playerFound;
+            objectToShow?.SetActive(isPlayerInside);
+        }
     }
+
+
+    // Maneja la acción según el tipo de objeto
+    void HandleInteraction(string tipo)
+    {
+        if (!Input.GetKeyDown(KeyCode.E) || string.IsNullOrEmpty(tipo)) return;
+
+        switch (tipo)
+        {
+            case "Chest":
+                if (!isOpened)
+                    OpenChest();
+                break;
+
+            case "Pistol":
+                GivePlayerPistol();
+                break;
+
+            case "Cargador":
+                GivePlayerAmmo();
+                break;
+
+            case "Money":
+                GivePlayerMoney();
+                break;
+        }
     }
+
+
+    // -----------------------
+    // FUNCIONES MODULARES
+    // -----------------------
+    void OpenChest()
+    {
+        isOpened = true;
+
+        // Animación
+        Animator anim = GetComponent<Animator>();
+        if (anim != null)
+            anim.SetTrigger("Abrir");
+
+        // Spawn de objetos
+        Vector3 spawnBase = transform.position + Vector3.up * -0.8f;
+
+        if (moneyPrefab != null)
+            Instantiate(moneyPrefab, spawnBase + Vector3.left * 0.3f, Quaternion.identity);
+
+        if (cargadorPrefab != null)
+            Instantiate(cargadorPrefab, spawnBase + Vector3.right * 0.3f, Quaternion.identity);
+
+        // Desactivar interacción futura
+        objectToShow?.SetActive(false);
+        triggerZone.enabled = false;
+
+    }
+    void GivePlayerPistol()
+    {
+        PlayerAttackScript.pistola = true;
+        PlayerAttackScript.yaTienePistola = true;
+        Destroy(gameObject);
+    }
+    void GivePlayerAmmo()
+    {
+        if (!PlayerAttackScript.pistola) return;
+
+        PlayerAttackScript.cargadores += 1;
+        PlayerAttackScript.TextoTMPCargadores.text = PlayerAttackScript.cargadores.ToString();
+        Destroy(gameObject);
+    }
+    void GivePlayerMoney()
+    {
+        MoneyScript.Instance.AddRandomMoney();
+        Destroy(gameObject);
+    }
+}
 
 
 
