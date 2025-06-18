638c3b35947300b2f20e75b90387b48a5a8d817e
Ichigo21pro <aiglegon@gmail.com>
2025-06-18 15:24:47 +0100

    Sonido implementado

--- Assets/_Main/Scripts/Player/PlayerAnimations.cs
+++ Assets/_Main/Scripts/Player/PlayerAnimations.cs
@@ -4,19 +4,45 @@ using UnityEngine;
 
 public class PlayerAnimations : MonoBehaviour
 {
+    [Header("Animaciones")]
     public Animator animator;             // Asigna el Animator en el Inspector
     public PlayerMovement playerMovement; // Asigna el script de movimiento
     public PlayerAttack playerAttack; // Asigna script ataque
 
-
     private bool wasAttacking = false;
     private bool wasReloading = false;
+
+    [Header("Sonido Caminar")]
+    //movimiento
+    public AudioClip[] footstepClips; // Tus 10 sonidos de pasos
+    private AudioSource audioSource;
+    private float stepTimer = 0f;
+    public float stepInterval = 0.5f; // Tiempo entre pasos
+    public float footstepVolume = 0.8f;
+    [Header("Sonido Disparo")]
+    //disparo
+    public AudioClip disparoClip;
+    //Recarga
+    public AudioClip regargaClip;
+
+    [Header("Sonido Cuchillo")]
+    public AudioClip cuchilloClip;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+    }
     void Update()
     {
         // Movimiento
         if (playerMovement != null)
         {
             animator.SetBool("isMoving", playerMovement.isMoving);
+            if (playerMovement.isMoving)
+            {
+                HandleFootsteps();
+            }
         }
         if (playerAttack != null)
         {
@@ -26,12 +52,14 @@ public class PlayerAnimations : MonoBehaviour
 
         if (playerAttack != null && playerAttack.isAttacking && !wasAttacking)
         {
+
             animator.SetTrigger("attack");   // Activa la animación de ataque
             wasAttacking = true;
             StartCoroutine(ResetAttackFlag());
         }
         if (playerAttack != null && playerAttack.isAttackingPistol && !wasAttacking)
         {
+            ReproducirSonidoDisparo();
             animator.SetTrigger("attackPistol");   // Activa la animación de ataque
             wasAttacking = true;
             StartCoroutine(ResetAttackFlag());
@@ -41,6 +69,7 @@ public class PlayerAnimations : MonoBehaviour
         {
             if (playerAttack.isReloading && !wasReloading)
             {
+                ReproducirSonidoRecargar();
                 animator.SetTrigger("reload");
                 wasReloading = true;
                 StartCoroutine(ResetAttackFlag());
@@ -61,5 +90,50 @@ public class PlayerAnimations : MonoBehaviour
         playerAttack.isAttacking = false;
         playerAttack.isAttackingPistol = false;
         wasAttacking = false;
+    }
+
+
+    // sonido de movimiento al caminar
+
+    void HandleFootsteps()
+    {
+        stepTimer += Time.deltaTime;
+
+        if (stepTimer >= stepInterval && footstepClips.Length > 0)
+        {
+            int index = Random.Range(0, footstepClips.Length);
+            audioSource.PlayOneShot(footstepClips[index], footstepVolume);
+            stepTimer = 0f;
+        }
+    }
+
+    // disparo
+
+    void ReproducirSonidoDisparo()
+    {
+        if (disparoClip != null)
+        {
+            audioSource.PlayOneShot(disparoClip, 0.3f);
+        }
+    }
+
+    //recargar
+
+    void ReproducirSonidoRecargar()
+    {
+        if (disparoClip != null)
+        {
+            audioSource.PlayOneShot(regargaClip, 0.3f);
+        }
+    }
+
+    // cuchillo
+
+    void ReproducirSonidoCuchillo()
+    {
+        if (cuchilloClip != null)
+        {
+            audioSource.PlayOneShot(cuchilloClip, 0.3f);
+        }
     }
 }
